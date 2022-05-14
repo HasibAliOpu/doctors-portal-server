19e652ce60c4ce73a38c0d573597ee69b208eefa
H_A _Opu <wwwhasib4@gmail.com>
2022-05-14 10:56:45 +0600

    setup post api for booking the appointment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
+const { get } = require("express/lib/response");
 const app = express();
-require("dotenv").config;
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pvs3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -15,16 +16,33 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-
-function run() {
+async function run() {
   try {
-    client.connect();
-    console.log("connecting DB");
+    await client.connect();
+    const servicesCollection = client
+      .db("Doctors-Portal")
+      .collection("services");
+    const bookingsCollection = client
+      .db("Doctors-Portal")
+      .collection("bookings");
+
+    // Get all treatment
+    app.get("/service", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      await bookingsCollection.insertOne(booking);
+      res.send({ message: `Your appointment is Booked!` });
+    });
   } catch (error) {
     console.log(error);
   }
 }
-run();
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello Doctor uncle!");

9bcc8a0d9a0d5a6aeb49c8096a9542d669c0b8d9
H_A _Opu <wwwhasib4@gmail.com>
2022-05-17 11:14:03 +0600

    verify the admin put api and make get api for filter the user role

--- index.js
+++ index.js
@@ -127,15 +127,31 @@ async function run() {
       res.send({ result, token });
     });
 
-    // PUT api for make admin
-    app.put("/user/admin/:email", async (req, res) => {
+    // GET api for filtering admin
+    app.get("/user/:email", async (req, res) => {
       const email = req.params.email;
-      const filter = { email: email };
-      const updateDoc = {
-        $set: { role: "admin" },
-      };
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
+    // PUT api for make admin
+    app.put("/user/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requestAccount = await usersCollection.findOne({
+        email: requester,
+      });
+      if (requestAccount.role === "admin") {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: "admin" },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      } else {
+        res.status(403).send({ message: "Forbidden Access" });
+      }
     });
   } catch (error) {
     console.log(error);
